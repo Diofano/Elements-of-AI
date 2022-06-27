af2fb660d59cc3ea1cdcb2c1c2f3d55e9bb35aec
Raihan <92286596+Diofano@users.noreply.github.com>
2022-06-27 12:07:23 +0700

    Create project.py

---
+++ project.py
@@ -0,0 +1,14 @@
+def main():
+   countries = ['Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden']
+   pop = [5615000, 5439000, 324000, 5080000, 9609000]  
+   fishers = [1891, 2652, 3800, 11611, 1757]
+
+   totPop = sum(pop)
+   totFish = sum(fishers)
+
+   # write your solution here
+
+   for i in range(len(countries)):
+      print("%s %.2f%%" % (countries[i], 100.0))   
+
+main()
